90015986014d0a8375bca2a8544c7d079a9e83c9
Roberto Calvi <robertocalvi@Robertos-MacBook-Pro-2.local>
2021-03-30 16:15:20 -0700

    uml added

--- contracts/RealEstateCollectible.sol
+++ contracts/RealEstateCollectible.sol
@@ -3,9 +3,9 @@ pragma solidity 0.6.6;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract SimpleCollectible is ERC721 {
+contract RealEstateCollectible is ERC721 {
     uint256 public tokenCounter;
-    constructor () public ERC721 ("Dogie", "DOG"){
+    constructor () public ERC721 ("RealEstateCollectible", "REC"){
         tokenCounter = 0;
     }
 
@@ -16,5 +16,6 @@ contract SimpleCollectible is ERC721 {
         tokenCounter = tokenCounter + 1;
         return newItemId;
     }
+
 
 }
